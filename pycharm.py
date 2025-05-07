bc57bbdc58f2e40900acf727b32b627a7b774628
rajdeep-360 <rajdeep.360@gmail.com>
2025-05-07 18:23:25 +0530

    pycharm.py
    Assignment 2

--- pycharm.py
+++ pycharm.py
@@ -1,16 +1,16 @@
 
-#Task 1
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+###Assignment_2
 
-print("Addition : ", num1 + num2)
-print("Subtraction : ", num1 - num2)
-print("Multiplication : ", num1 * num2)
-print("Division : ", num1 / num2)
+#Task 1
+num1 = int(input("Enter a number: "))
+if num1 % 2 == 0:
+    print(num1, "is an even number")
+else:
+    print(num1, "is an odd number")
+
 
 #Task 2
-
-fstname = str(input("Enter the first name: "))
-lstname = str(input("Enter the last name: "))
-
-print(f" Hello, {fstname} {lstname}! Welcome to the python program")
+sum1 = 0
+for i in range(1, 51):
+    sum1 += i
+print("The sum of numbers from 1 to 50 is:", sum1)
