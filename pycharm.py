9c275d4bf9c6c30ca41fb910c5a4af6b5d8ae73f
rajdeep-360 <rajdeep.360@gmail.com>
2025-05-07 17:48:21 +0530

    Add files via upload

---
+++ pycharm.py
@@ -0,0 +1,16 @@
+
+#Task 1
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("Addition : ", num1 + num2)
+print("Subtraction : ", num1 - num2)
+print("Multiplication : ", num1 * num2)
+print("Division : ", num1 / num2)
+
+#Task 2
+
+fstname = str(input("Enter the first name: "))
+lstname = str(input("Enter the last name: "))
+
+print(f" Hello, {fstname} {lstname}! Welcome to the python program")
